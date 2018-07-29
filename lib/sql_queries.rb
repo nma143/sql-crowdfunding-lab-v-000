6e7130158450d7d096bee7998ee95479060a8979
Nikki A <nikki.apostolakis@gmail.com>
2018-07-29 01:19:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.id, SUM(pledges.amount) FROM projects LEFT OUTER JOIN projects.id=pledges.project_id GROUP BY project_id"
-"SELECT project_id, SUM(amount) FROM pledges GROUP BY project_id"
+# "SELECT project_id, SUM(amount) FROM pledges GROUP BY project_id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
