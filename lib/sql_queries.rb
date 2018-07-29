eaa9c5afeeed69fea78c8981ac99f93ffdb71e5c
Nikki A <nikki.apostolakis@gmail.com>
2018-07-29 01:47:30 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,26 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) FROM projects LEFT OUTER JOIN pledges ON projects.id=pledges.project_id GROUP BY project_id ORDER BY projects.title"
+# "SELECT project_id, SUM(amount) FROM pledges GROUP BY project_id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY user_id ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal FROM projects LEFT OUTER JOIN pledges ON projects.id=pledges.project_id GROUP BY project_id HAVING SUM(pledges.amount) >= projects.funding_goal ORDER BY projects.title "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) FROM users LEFT OUTER JOIN pledges ON users.id = pledges.user_id GROUP BY user_id ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount FROM projects LEFT OUTER JOIN pledges on projects.id=pledges.project_id WHERE projects.category='music' ORDER BY pledges.id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM projects LEFT OUTER JOIN pledges on projects.id=pledges.project_id WHERE projects.category='books' ORDER BY pledges.id"
 end
